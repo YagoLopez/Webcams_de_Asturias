fcdfce0184bb4ea6b638d13f5dbe3cba8342d3e7
Yago López <yago.lopez@gmail.com>
2015-09-18 12:55:28 +0200

    prueba de commit 2

---
+++ www/js/services.js
@@ -0,0 +1,5 @@
+//var url_api = "https://www.googleapis.com/fusiontables/v2/query?sql=SELECT%20*%20FROM%201gX5maFbqFyRziZiUYlpOBYhcC1v9lGkKqCXvZREF&key=AIzaSyBsdouSTimjrC2xHmbGgOt8VfbLBWc9Gps";
+//
+//angular.factory('mifactoria', function() {
+//
+//})
