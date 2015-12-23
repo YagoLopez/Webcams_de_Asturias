4dc0435dfc833ab30bc120e2fd07df199d01d0b7
YagoLopez <yago.lopez@gmail.com>
2015-12-23 03:42:28 +0100

    App funcionando con el emulador phonegap. Falta arreglar detalles y optimizar. Se han tenido que eliminar las librerias angular material y algunas más.

--- www/js/app.js
+++ www/js/app.js
@@ -2,7 +2,7 @@
 //var url_api = "https://www.googleapis.com/fusiontables/v2/query?sql=SELECT%20*%20FROM%201gX5maFbqFyRziZiUYlpOBYhcC1v9lGkKqCXvZREF&key=AIzaSyBsdouSTimjrC2xHmbGgOt8VfbLBWc9Gps";
 
 angular.module('wca', ['ionic', 'wca.controllers', 'wca.services',
-  'jett.ionic.filter.bar', 'ngMaterial' /*'ionicLazyLoad'*/])
+  'jett.ionic.filter.bar'/*, 'ngMaterial' 'ionicLazyLoad'*/])
 
 .run(function($ionicPlatform) {
   $ionicPlatform.ready(function() {
