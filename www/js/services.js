1c0578a435cdf78f0ba8d403be71051881ba7213
YagoLopez <yago.lopez@gmail.com>
2017-10-24 23:17:44 +0200

    Refactors in "gif-player.html" buttons and others

--- www/js/services.js
+++ www/js/services.js
@@ -1,6 +1,5 @@
 // Url completa para consultar fusion table. Usar como plantilla
 // https://www.googleapis.com/fusiontables/v2/query?sql=SELECT%20*%20FROM%201gX5maFbqFyRziZiUYlpOBYhcC1v9lGkKqCXvZREF&key=AIzaSyBsdouSTimjrC2xHmbGgOt8VfbLBWc9Gps
-
 var wcaModule = angular.module('wca.services',[]);
 // ====================================================================================================================
 /*
