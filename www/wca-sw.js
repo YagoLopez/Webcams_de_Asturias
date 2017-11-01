2d3829f24203dec83f8d0e0283de77b776170cf6
YagoLopez <yago.lopez@gmail.com>
2017-11-01 21:26:05 +0100

    Refactors in service worker

--- www/wca-sw.js
+++ www/wca-sw.js
@@ -73,8 +73,8 @@ var filesToCache = [
  * Service worker registration
  */
 if ('serviceWorker' in navigator) {
-  navigator.serviceWorker.register('wca-sw.js', {scope: '/Webcams_de_Asturias/www/'}).then(function() {
-  // navigator.serviceWorker.register('wca-sw.js').then(function() {
+  // navigator.serviceWorker.register('wca-sw.js', {scope: '/Webcams_de_Asturias/www/'}).then(function() {
+  navigator.serviceWorker.register('wca-sw.js').then(function() {
     console.log('sw: registration ok');
   }).catch(function(err) {
     console.error(err);
